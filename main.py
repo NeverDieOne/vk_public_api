c2d42b4a8f0e10cb54c0646f60ec4239a8453fe9
NeverDieOne <altiore@mail.ru>
2019-04-26 17:05:36 +0300

    Add function to get info about a picture. Add function to download a picture.

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-def main():
-    print('Hello!')
+import requests
+
+
+def get_picture_data(picture_id):
+    url = f'http://xkcd.com/{picture_id}/info.0.json'
+    return requests.get(url).json()
+
+
+def download_picture(picture_id):
+    picture_link = get_picture_data(picture_id)['img']
+
+    picture_response = requests.get(picture_link)
+    filename = f'{picture_id}.png'
+    with open(filename, 'wb') as image:
+        image.write(picture_response.content)
 
 
 if __name__ == '__main__':
-    main()
+    print(get_picture_data(312)['alt'])
