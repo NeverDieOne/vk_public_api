44256eb41d2fd3ebeea53229ff1c6fae6c8b6056
NeverDieOne <altiore@mail.ru>
2019-04-27 14:33:03 +0300

    Add dictionaries for all GET/POST params.

--- main.py
+++ main.py
@@ -19,9 +19,13 @@ def download_picture(picture_id):
 
 
 def get_info_for_upload(group_id, *args):
-    params = f'group_id={group_id}'
-    url = f'https://api.vk.com/method/photos.getWallUploadServer?{params}&access_token={ACCESS_TOKEN}&v=5.95'
-    response = requests.get(url).json()
+    params = {
+        'group_id': group_id,
+        'access_token': ACCESS_TOKEN,
+        'v': '5.95'
+    }
+    url = f'https://api.vk.com/method/photos.getWallUploadServer'
+    response = requests.get(url, params=params).json()
     return response['response'][args[0]]
 
 
@@ -37,22 +41,31 @@ def upload_picture(pic_num, group_id):
 
 def get_data_for_post(pic_num, group_id):
     server, photo, _hash = upload_picture(pic_num, group_id)
-    params = f'server={server}&photo={photo}&hash={_hash}&group_id={group_id}'
-
-    url = f'https://api.vk.com/method/photos.saveWallPhoto?{params}&access_token={ACCESS_TOKEN}&v=5.95'
-    response = requests.post(url).json()
+    params = {
+        'server': server,
+        'photo': photo,
+        'hash': _hash,
+        'group_id': group_id,
+        'access_token': ACCESS_TOKEN,
+        'v': '5.95'
+    }
+    url = f'https://api.vk.com/method/photos.saveWallPhoto'
+    response = requests.post(url, params=params).json()
     return response['response'][0]['id'], response['response'][0]['owner_id']
 
 
 def post_picture(pic_num, group_id):
     media_id, owner_id = get_data_for_post(pic_num, group_id)
-    attachments = f'photo{owner_id}_{media_id}'
-    message = get_picture_data(pic_num)['alt']
-    owner_id = f'-{group_id}'
-    params = f'owner_id={owner_id}&from_group=1&attachments={attachments}&message={message}'
-
-    url = f'https://api.vk.com/method/wall.post?{params}&access_token={ACCESS_TOKEN}&v=5.95'
-    requests.get(url)
+    params = {
+        'owner_id': f'-{group_id}',
+        'message': get_picture_data(pic_num)['alt'],
+        'attachments': f'photo{owner_id}_{media_id}',
+        'from_group': 1,
+        'access_token': ACCESS_TOKEN,
+        'v': '5.95'
+    }
+    url = f'https://api.vk.com/method/wall.post'
+    requests.get(url, params=params)
     os.remove(f'{pic_num}.png')
 
 
